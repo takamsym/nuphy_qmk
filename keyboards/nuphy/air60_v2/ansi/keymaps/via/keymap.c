5ea0b9c133258cde9b2d1c0491065356df65082a
daiv <disposable.tkmsym@gmail.com>
2026-05-19 11:30:38 +0900

    esc, bs, backspaceを変更

--- keyboards/nuphy/air60_v2/ansi/keymaps/via/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/via/keymap.c
@@ -46,18 +46,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // layer 3 Win
 [3] = LAYOUT(
-	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 	KC_BSLS,
-	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 	KC_BSPC,
+	KC_ESC, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,
+	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 	KC_BSLS,
 	KC_LCTL,	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	            KC_ENT,
-	KC_LSFT,	KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,	KC_UP,		KC_DEL,
+	KC_LSFT,	KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,	KC_UP,		KC_RSFT,
 	MO(4),		KC_LGUI,	KC_LALT,										KC_SPC, 							MO(4),		KC_RALT,   	KC_LEFT,	KC_DOWN,    KC_RGHT),
 
 // layer 4 win fn
 [4] = LAYOUT(
-	KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12,		_______,
-	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 	KC_DEL,
+	KC_GRV, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12,		KC_DEL,
+	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 	_______,
 	_______, 	_______,   	_______,   	KC_INT5,  	KC_INT4,   	_______,   	KC_LEFT,	KC_DOWN,   	KC_UP,   	KC_RGHT,  	_______,   	_______,	            _______,
-	KC_LSFT,    _______,   	_______,   	_______,  	_______,   	_______,   	KC_HOME,	KC_PGDN,   	KC_PGUP,	KC_END,  	_______,	KC_RSFT,	KC_PGUP,    _______,
+	KC_LSFT,    _______,   	_______,   	_______,  	_______,   	_______,   	KC_HOME,	KC_PGDN,   	KC_PGUP,	KC_END,  	_______,	KC_RSFT,	KC_PGUP,    KC_RSFT,
 	MO(5),		MO(6),		_______,										_______, 							_______,	_______,   	KC_HOME,	KC_PGDN,    KC_END),
 
 // layer 5 win fn+shift
